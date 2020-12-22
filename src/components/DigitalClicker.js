b7c6c3df989964fb087684aa83ee6e3f2fdc5620
sandienunez <github email address>
2020-12-22 16:44:30 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -32,3 +32,31 @@ class DigitalClicker extends React.Component {
 }
 
 export default DigitalClicker;  
+
+//flatiron way to solve it: 
+
+// import React from 'react';
+
+// class DigitalClicker extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       timesClicked: 0,
+//     };
+//   }
+
+//   handleClick = () => {
+//     this.setState(prevState => ({
+//       timesClicked: prevState.timesClicked+1
+//     }))
+//   }
+
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+//     );
+//   }
+// }
+
+// export default DigitalClicker;
